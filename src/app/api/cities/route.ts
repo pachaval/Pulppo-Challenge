e50a3053fe456ba01b8e2579f6f5674924d127f6
pabloachaval@cookunity.com <pabloachaval@gmail.com>
2024-12-11 15:22:09 -0300

    fix: get cities route

--- src/app/api/cities/route.ts
+++ src/app/api/cities/route.ts
@@ -9,13 +9,41 @@ export async function GET() {
   try {
     await connectToDatabase();
 
-    const uniqueCities = await Cities.distinct("address.city.name", {
-      "address.city.name": { $exists: true },
-      type: { $in: ["Casa", "Departamento"] },
-      "attributes.roofedSurface": { $gt: 0 },
-    });
+    const uniqueCities = await Cities.aggregate([
+      {
+        $match: {
+          "address.city.name": { $exists: true },
+          type: { $in: ["Casa", "Departamento"] },
+          "attributes.roofedSurface": { $gt: 0 },
+        },
+      },
+      {
+        $group: {
+          _id: "$address.city.name",
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $match: {
+          count: { $gt: 30 },
+        },
+      },
+      {
+        $sort: {
+          _id: 1,
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          city: "$_id",
+        },
+      },
+    ]);
 
-    return new Response(JSON.stringify(uniqueCities), { status: 200 });
+    const cityNames = uniqueCities.map((entry) => entry.city);
+
+    return new Response(JSON.stringify(cityNames), { status: 200 });
   } catch (error: any) {
     return new Response(JSON.stringify({ error: error.message }), {
       status: 500,
