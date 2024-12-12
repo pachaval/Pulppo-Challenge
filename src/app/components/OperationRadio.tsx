7e25ac49ac0ae538d14c5bb4d3d4c83bb02fc5f6
pabloachaval@cookunity.com <pabloachaval@gmail.com>
2024-12-12 17:51:12 -0300

    fix: operation radio warning

--- src/app/components/OperationRadio.tsx
+++ src/app/components/OperationRadio.tsx
@@ -8,40 +8,41 @@ import {
 const OperationRadio = () => {
   const [{ operation }, setOperation] =
     useAtom<OperationRadioState>(operationRadioAtom);
+
+  const handleChange = (newOperation: "sale" | "rent") => {
+    setOperation({ operation: newOperation });
+  };
+
   return (
     <div className="flex-col">
-      <div
-        onClick={() => setOperation({ operation: "sale" })}
-        className="flex items-center mb-4"
-      >
+      <div className="flex items-center mb-4">
         <input
           className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
           checked={operation === "sale"}
           type="radio"
-          name="sale"
+          name="operation"
           id="sale"
+          onChange={() => handleChange("sale")}
         />
         <label
           className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-          htmlFor="default-radio-1"
+          htmlFor="sale"
         >
           Venta
         </label>
       </div>
-      <div
-        onClick={() => setOperation({ operation: "rent" })}
-        className="flex items-center"
-      >
+      <div className="flex items-center">
         <input
           className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
           checked={operation === "rent"}
           type="radio"
-          name="rent"
+          name="operation"
           id="rent"
+          onChange={() => handleChange("rent")}
         />
         <label
           className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-          htmlFor="default-radio-2"
+          htmlFor="rent"
         >
           Alquiler
         </label>
